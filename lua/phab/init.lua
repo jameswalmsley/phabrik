a71c80ff4210950b8dec560963de27472afb867d
James Walmsley <james@fullfat-fs.co.uk>
2021-01-11 17:03:19 +0000

    Add support for updating points!

--- lua/phab/init.lua
+++ lua/phab/init.lua
@@ -2,17 +2,34 @@ local function get_path()
 	return vim.g.phab_path:match("^(.+)/.+$"):match("^(.+)/.+$")
 end
 
-local function update_task()
-	local path = get_path()
+local function get_file_path()
 	local file_reg = vim.api.nvim_eval('@%')
-	name = file_reg:match("^.+/(.+)%..+$")
-
-	vim.api.nvim_command("!python3 " .. path .. "/py/task.py " .. name .. " " .. file_reg)
+	return file_reg
 end
 
+local function get_task_id()
+	local file_reg = get_file_path()
+	name = file_reg:match("^.+/(.+)%..+$")
+	return name
+end
+
+local function tasks_command(command, task, args)
+	vim.api.nvim_command("!python3 " .. get_path() .. "/py/task.py " .. command .. " " .. task .. " " .. args)
+end
+
+local function update_task()
+	tasks_command("description", get_task_id(), get_file_path())
+end
+
+local function update_points()
+
+	points = vim.fn.input("How many points? > ")
+	tasks_command("points", get_task_id(), points)
+end
 
 return {
 	update_task = update_task,
+	update_points = update_points,
 	get_path = get_path
 }
 
--- py/task.py
+++ py/task.py
@@ -3,26 +3,33 @@ import sys
 
 from phabricator import Phabricator
 
-task = sys.argv[1]
-file = sys.argv[2]
+cmd = sys.argv[1]
+task = sys.argv[2]
+arg = sys.argv[3]
 
 if not task.startswith('T'):
     print("Error invalid task number -> begin with Txxxxx")
     exit(1)
 
-num = int(task[1:])
+transactions = []
 
+if cmd == "description":
+    description=""
 
-description=""
+    with open(arg, 'r') as fp:
+        for cnt, line in enumerate(fp):
+            matches = re.findall('(\[\[T\d+\]\])', line)
+            if matches:
+                for t in matches:
+                    line = line.replace(t, t.replace('[','').replace(']', ''))
+            description = description + line
 
-with open(file, 'r') as fp:
-    for cnt, line in enumerate(fp):
-        matches = re.findall('(\[\[T\d+\]\])', line)
-        if matches:
-            for t in matches:
-                line = line.replace(t, t.replace('[','').replace(']', ''))
-        description = description + line
+    transactions.append({'type':'description',  'value':description})
+
+if cmd == "points":
+    transactions.append({'type':'points',  'value':arg})
 
 phab = Phabricator();
-task = phab.maniphest.update(id=num, description=description)
+
+task = phab.maniphest.edit(objectIdentifier=task, transactions=transactions)
 
